03267d3afbbf14e06975637575e607dc834ba104
guschi18 <philipp.guschl@gmx.de>
2024-08-17 13:46:48 +0200

    Update AboutSection

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -4,14 +4,14 @@
 function AboutSection() {
   
    return (
-    <section className="flex justify-center text-white" id="about">
+    <section className="flex justify-center text-white xl:pb-20" id="about">
     <div className="gap-8 flex items-center justify-center px-4
     lg:w-[850px] lg:h-[450px]
     xl:w-[1550px] xl:h-[550px]
     xl:px-16 ">
-        <div className=" text-left flex flex-col justify-center xl:mt-28 xl:pt-16 mt-12 pt-2">
-        <h2 className=" md:text-4xl lg:text-5xl text-4xl font-bold text-white flex justify-center pl-4 pr-4 mr-2">Herzlich</h2>
-        <h2 className=" md:text-4xl lg:text-5xl text-4xl font-bold text-white flex justify-center pl-4 pb-6 pr-4">Willkommen</h2>
+        <div className=" text-left flex flex-col justify-center xl:mt-10 xl:pt-16 mt-12 pt-2">
+        <h2 className=" md:text-4xl lg:text-5xl text-4xl font-bold text-white flex justify-center pl-2 pr-4 mr-2">Herzlich</h2>
+        <h2 className=" md:text-4xl lg:text-5xl text-4xl font-bold text-white flex justify-center pl-2 pb-6 pr-4">Willkommen</h2>
         <p className='text-white text-xl lg:text-2xl xl:mt-12'> bei JP Caravans! Wir sind dein Partner im Bereich des autarken Campens und weiterem Zubehör für deinen Caravan.
         <br/>
         <br/>
