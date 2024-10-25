cb8c7eddb4b7f188c50b406e7869ec3d7e659ee5
guschi18 <philipp.guschl@gmx.de>
2024-10-25 15:35:32 +0200

    Text Anpassung

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -12,10 +12,10 @@ function AboutSection() {
         <div className=" text-left flex flex-col justify-center xl:mt-10 xl:pt-16 mt-12 pt-2">
         <h2 className=" md:text-4xl lg:text-5xl text-4xl font-bold text-white flex justify-center pl-2 pr-4 mr-2">Herzlich</h2>
         <h2 className=" md:text-4xl lg:text-5xl text-4xl font-bold text-white flex justify-center pl-2 pb-6 pr-4">Willkommen</h2>
-        <p className='text-white text-xl lg:text-2xl xl:mt-12'> bei JP Caravans! Wir sind dein Partner im Bereich des autarken Campens und weiterem Zubehör für deinen Caravan in der Region Gifhorn, Wolfsburg und Braunschweig.
+        <p className='text-white text-xl lg:text-2xl xl:mt-12'> bei JP Caravans! Wir sind dein Partner im Bereich des autarken Campens und weiterem Zubehör für deinen Wohnwagen, Wohnmobil oder Van in der Region Gifhorn, Wolfsburg und Braunschweig.
         <br/>
         <br/>
-        Egal ob Wohnwagen, Wohnmobil oder Van – mit uns erschaffst du deinen ganz persönlichen Campertraum. Ob du einen Mover nachgerüstet haben möchtest oder dein Camper komplett autark werden soll – bei uns erhältst du alles, was du für deinen Caravan brauchst!
+        Mit uns erschaffst du deinen ganz persönlichen Campertraum. Ob du einen Mover nachgerüstet haben möchtest oder dein Camper komplett autark werden soll – bei uns erhältst du alles, was du für deinen Caravan brauchst!
         <br/>
         <br/>
         Wir, das sind Julian und Philipp, zwei seit Jahren begeisterte Camper, die ihre eigenen Camper schon individuell gestaltet haben. Da wir beide Kfz-Meister sind und auf jahrelange Berufserfahrung zurückblicken können, ist dein Camper bei uns in besten Händen!
