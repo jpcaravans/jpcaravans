9b8529d822b432090efa033859884f002dffd57f
guschi18 <philipp.guschl@gmx.de>
2024-10-25 14:18:08 +0200

    +head

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 import AboutSection from './components/AboutSection'
@@ -6,10 +5,19 @@ import ProjectsSection from './components/ProjectsSection'
 import EmailSection from './components/EmailSection'
 import Footer from './components/Footer'
 import AchievementsSection from './components/AchievementsSection'
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
+    <>
+    <Head>
+        <meta
+          name="google-site-verification"
+          content="F1hmPNsQbG0T5w5diUh6oWUubUy03V34bWcfUc0_7is"
+        />
+        <title>JP Caravans dein Profi für Nachrüstungen von Movern Solaranlagen und mehr</title>
+      </Head>
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
@@ -22,5 +30,6 @@ export default function Home() {
       <Footer />
       
     </main>
+    </>
   )
 }
