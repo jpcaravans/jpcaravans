dc6a15c2fb12e3d01d629672db9ff23b039eba67
guschi18 <philipp.guschl@gmx.de>
2024-10-25 14:24:09 +0200

    +head3

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="F1hmPNsQbG0T5w5diUh6oWUubUy03V34bWcfUc0_7is"
+        />
+        <title>JP Caravans dein Profi für Nachrüstungen von Movern Solaranlagen und mehr</title>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
