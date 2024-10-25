84ba4757ee2ffb5bf77420f5e6bca88bf998232a
guschi18 <philipp.guschl@gmx.de>
2024-10-25 14:22:49 +0200

    +head2

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'JP Caravans',
   description: 'Dein Profi für Campingnachrüstungen',
+  name: "google-site-verification",
+  content: "F1hmPNsQbG0T5w5diUh6oWUubUy03V34bWcfUc0_7is"
 }
 
 export default function RootLayout({ children }) {
